77708b72a544c70291d5ddd5cddd75d2eac42207
NipuniHerath <nipunikaherath9@gmail.com>
2023-03-08 14:40:16 +0530

    create user and client model

---
+++ src/main/java/com/example/apassingment/Client.java
@@ -0,0 +1,52 @@
+package com.example.apassingment;
+
+public class Client extends User{
+    private String fname;
+    private String lname;
+    private String address;
+    private String dob;
+    private String nic;
+
+    public Client() {
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getNic() {
+        return nic;
+    }
+
+    public void setNic(String nic) {
+        this.nic = nic;
+    }
+}
