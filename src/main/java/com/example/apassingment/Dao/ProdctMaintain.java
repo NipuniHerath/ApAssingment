127c26bb856671ea72eda83f1f6a95aec2368701
NipuniHerath <nipunikaherath9@gmail.com>
2023-03-09 13:07:38 +0530

    view update function

--- src/main/java/com/example/apassingment/Dao/ProdctMaintain.java
+++ src/main/java/com/example/apassingment/Dao/ProdctMaintain.java
@@ -2,10 +2,9 @@ package com.example.apassingment.Dao;
 
 import com.example.apassingment.model.product;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
+import java.util.List;
 
 public class ProdctMaintain  {
     public static boolean createproduct(product product) throws SQLException, ClassNotFoundException {
@@ -25,5 +24,30 @@ public class ProdctMaintain  {
         PreparedStatement ps = connection.prepareStatement(query);
         boolean result=ps.executeUpdate()>0;
         return result;
+    }
+
+    public static boolean updateProduct(product product) throws SQLException, ClassNotFoundException {
+        GetConnection getConnection  =  new Database();
+        Connection connection = getConnection. con();
+        String query="";
+        PreparedStatement ps = connection.prepareStatement(query);
+        boolean result=ps.executeUpdate()>0;
+        return result;
+    }
+
+    public static List<product> ProductList() throws SQLException, ClassNotFoundException {
+        GetConnection getConnection  =  new Database();
+        Connection connection = getConnection. con();
+        String query="";
+        Statement st = connection.createStatement();
+        ResultSet resultSet  = st.executeQuery(query);
+        List<product>productsArray = new ArrayList<product>();
+        while (resultSet.next())
+        {
+            product product = new product();
+            productsArray.add(product);
+        }
+
+        return productsArray;
     }
 }
